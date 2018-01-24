465c5c069ef6eaee83423848f5deafee82ff2431
Mark Feinman <feinmanmarke@gmail.com>
2018-01-24 13:32:05 -0700

    revised commit 2nd)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,47 +12,60 @@ import java.util.Scanner;
 public class Controller {
 
     @FXML
-            private Text guessGameText;
+    private Text guessGameText;
 
     @FXML
 
     private Text questionText;
 
     @FXML
-            private Button submitButton;
+    private Button submitButton;
 
     @FXML
-            private TextField userInput;
+    private TextField userInput;
 
     @FXML
-            private Text responseText;
+    private Text responseText;
 
 
-        Scanner sc = new Scanner(System.in);
-        Random generator = new Random();
+    Scanner sc = new Scanner(System.in);
+    Random generator = new Random();
 
-        int MIN = 1;
-        int MAX = 5;
-        int answer, guess;
-        int numberOfGuesses = 0;
-        int guesses;
-        String another;
-        boolean flag;
-        boolean anotherFlag = true;
+    int MIN = 1;
+    int MAX = 5;
+    int answer, guess;
+    int numberOfGuesses = 0;
+    int guesses;
+    String another;
+    boolean flag;
+    boolean anotherFlag = true;
 
 
-public void clickedTheButton (ActionEvent actionEvent){
-    answer = generator.nextInt(MAX) + 1;
-    String userEntry = userInput.getText();
+    public void clickedTheButton(ActionEvent actionEvent) {
+        answer = generator.nextInt(MAX) + 1;
+        String userEntry = userInput.getText();
+        int userInput = Integer.parseInt(userEntry);
+        guess = userInput;
 
-    //questionText.setText("I'm thinking of a number");
+        //questionText.setText("I'm thinking of a number");
 
+        if (guess == answer) {
+            responseText.setText("That's correct, Great Job!!!");
+        } else if (guess > answer) {
+            responseText.setText("Too High, Guess again");
+        } else if (guess < answer) {
+            responseText.setText("Too Low, Guess again");
+        }
+
+    }
 
 }
 
 
 
 
-    }
+
+
+
 
 
