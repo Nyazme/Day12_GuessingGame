d7d4517ccdf0be67d935175b8dbdc2a6147c91f1
Mark Feinman <feinmanmarke@gmail.com>
2018-01-24 14:09:35 -0700

    revision 3

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -32,7 +32,7 @@ public class Controller {
     Random generator = new Random();
 
     int MIN = 1;
-    int MAX = 5;
+    int MAX = 10;
     int answer, guess;
     int numberOfGuesses = 0;
     int guesses;
@@ -46,11 +46,13 @@ public class Controller {
         String userEntry = userInput.getText();
         int userInput = Integer.parseInt(userEntry);
         guess = userInput;
+        numberOfGuesses=numberOfGuesses + 1; //counter
 
         //questionText.setText("I'm thinking of a number");
 
         if (guess == answer) {
-            responseText.setText("That's correct, Great Job!!!");
+            responseText.setText("                                 That's correct, Great Job!!!");
+            questionText.setText("It took you " + numberOfGuesses + " tries to guess the number.");
         } else if (guess > answer) {
             responseText.setText("Too High, Guess again");
         } else if (guess < answer) {
